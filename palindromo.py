077c190ba1288f1d6c5ed03a1913bb6efa1b8e4b
Mon Avellaneda <mon@mon-avellaneda.community>
2021-11-01 18:14:50 +0100

    Codigo del palindromo

--- palindromo.py
+++ palindromo.py
@@ -1,6 +1,11 @@
 def palindromo(palabra):
     palabra = palabra.replace(" ", "")
-    
+    palabra = palabra.lower()
+    palabra_invertida = palabra[::-1]
+    if palabra == palabra_invertida: 
+        return True
+    else: 
+        return False
 
 
 def run():
